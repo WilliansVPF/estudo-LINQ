c10a3f282f281f3eef67c63a1370b464a51b388f
WilliansVPF <williansvpf@gmail.com>
2025-07-21 11:55:02 -0300

    exemplo de filtro Query Syntax

---
+++ QuerySyntax/Filtros.cs
@@ -0,0 +1,26 @@
+namespace estudoLINQ.QuerySyntax;
+
+public class Filtro
+{
+    private static List<Despesa> despesas = new List<Despesa>
+    {
+        new Despesa { Descricao = "Internet", Valor = 100, Vencimento = new DateTime(2021, 10, 1) },
+        new Despesa { Descricao = "Celular", Valor = 80, Vencimento = new DateTime(2021, 10, 8) },
+        new Despesa { Descricao = "Aluguel", Valor = 1200, Vencimento = new DateTime(2021, 10, 18) },
+        new Despesa { Descricao = "Contabilidade", Valor = 400, Vencimento = new DateTime(2021, 10, 20) }
+    };
+
+    public static void filtro()
+    {
+        var despesasVencidas = from d in despesas //d = variavel de interação despesas = fonte de dados
+                               where d.Valor < 500 && d.Vencimento <= new DateTime(2021, 10, 10) // filtro
+                               select d; // retorno
+
+
+        foreach (var d in despesasVencidas)
+        {
+            Console.WriteLine(d);
+        }
+    }
+
+}
