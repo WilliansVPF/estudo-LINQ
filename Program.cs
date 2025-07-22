f4a06b8aced3e18a612cafa7507afaa7dcc20834
WilliansVPF <williansvpf@gmail.com>
2025-07-22 18:16:55 -0300

    chamada dos exemplos

--- Program.cs
+++ Program.cs
@@ -9,4 +9,15 @@ using estudoLINQ.MethodSyntax;
 //* exemplo de Method Syntax
 // VerificaOcorrencia.verificaOcorrencia(); //? verifica ocorrencia de itens
 // FiltroMethod.filtro(); //? filtro
-RecuperaUmElemento.recuperaUmElemento(); //? recupera apenas um elemento
+// RecuperaUmElemento.recuperaUmElemento(); //? recupera apenas um elemento
+// LimiteQuantidadeResultado.limiteQuantidadeResultado(); //? Limita a quantidade de registro retornado
+// AvancarRegistro.avancarRegistro(); //? pula registro
+// OrdenarResultado.ordenaCrescente(); //? ordena de forma crescente
+// OrdenarResultado.ordenaDecrescente(); //? ordena de forma decrescente
+// OrdenacaoSecundaria.ordenacaoSecundariaCrescente(); //? ordenação secundaria crescente
+// OrdenacaoSecundaria.ordenacaoSecundariaDecrescente(); //? ordenação secundaria decrescente
+// AlterandoRetorno.alterandoRetorno(); //? altera o retorno da consulta
+// ResultadosDistintos.resultadosDistintos(); //? retorna apenas uma ocorrencia do item
+// FuncoesAgregacao.funcoesAgregacao(); //? agrega valores
+// AgrupandoResultados.agrupandoResultados(); //? agupa resultados Join
+UniaoConsultas.uniaoConsultas(); //? une duas coleções
