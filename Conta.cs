daae4ea3643792918ca3cb55dfff7ccbd3e5d13c
WilliansVPF <williansvpf@gmail.com>
2025-07-22 18:16:36 -0300

    adicionado fonte de dados

---
+++ Conta.cs
@@ -0,0 +1,10 @@
+namespace estudoLINQ;
+
+public class Conta
+    {
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Vencimento { get; set; }
+
+        public override string ToString() => $"{ Descricao.PadRight(20, ' ') } | { Vencimento.ToString("dd/MM/yyyy") } | { Valor.ToString("f").PadLeft(8, ' ') }";
+    }
