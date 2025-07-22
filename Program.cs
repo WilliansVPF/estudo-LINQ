8d8c3a7f953eae0ce33ba9a0eb8a3190bcd0a45d
WilliansVPF <williansvpf@gmail.com>
2025-07-21 21:44:37 -0300

    chamada dos exemplos

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿using estudoLINQ.QuerySyntax;
+using estudoLINQ.MethodSyntax;
 
-// Filtro.filtro(); //filtro
-// Ordenacao.ordenação(); //ordenação
-JuncaoDados.juncaoDados();
+//* exemplos de Query Syntax
+// Filtro.filtro(); //? filtro
+// Ordenacao.ordenação(); //? ordenação
+// JuncaoDados.juncaoDados(); //? junção de dados
+
+//* exemplo de Method Syntax
+// VerificaOcorrencia.verificaOcorrencia(); //? verifica ocorrencia de itens
+// FiltroMethod.filtro(); //? filtro
+RecuperaUmElemento.recuperaUmElemento(); //? recupera apenas um elemento
